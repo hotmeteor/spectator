6b78b2dd122a29d687d41250abb383b25bdfe005
clair-rjacobsen <79278968+clair-rjacobsen@users.noreply.github.com>
2021-05-12 15:50:50 -0400

    Rjacobsen updates (#1)
    * update to make methods available without searching, and add .idea to .gitignore
    
    * allow passing of spec and path

--- src/Spectator.php
+++ src/Spectator.php
@@ -4,6 +4,16 @@ namespace Spectator;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Spectator
+ *
+ * @method static void using($name): void
+ * @method static void reset(): void
+ * @method static void getSpec(): string|null
+ * @method static void resolve(): SpecObjectInterface
+ *
+ * @package Spectator
+ */
 class Spectator extends Facade
 {
     /**
