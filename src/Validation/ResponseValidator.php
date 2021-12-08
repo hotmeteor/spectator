ef93ce4a9d75fcc7540e2999a62f143f2d873c87
Jarrod Parkes <parkesfjarrod@gmail.com>
2021-12-08 12:14:54 -0600

    stop gap for mvp functionality

--- src/Validation/ResponseValidator.php
+++ src/Validation/ResponseValidator.php
@@ -83,129 +83,8 @@ class ResponseValidator extends AbstractValidator
 
         if ($result instanceof ValidationResult && $result->isValid() === false) {
             $error = $result->error();
-            $message = $this->display_validation_error($expected_schema, $error);
-
+            $message = $this->validation_error_message($expected_schema, $error);
             throw ResponseValidationException::withError($message, $error);
-        }
-    }
-
-    protected function display_validation_error($expected_schema, $error)
-    {
-        $formatter = new ErrorFormatter();
-        $spec_errors = join("\n", $formatter->formatFlat($error));
-        $spec_errors .= "\n\n".json_encode($formatter->formatOutput($error, "basic"));
-
-        $expected_schema_json = json_decode(json_encode($expected_schema), true);
-        $expected_schema_pretty = $this->pretty_print($expected_schema_json, "", [], 0);
-
-        $expected_schema_pretty_final = join("\n", $expected_schema_pretty);
-        
-        return "---\n\n".$spec_errors."\n\n".$expected_schema_pretty_final."\n\n  ---";
-    }
-
-    protected function pretty_print($json, $key, $required_keys, $indent_level)
-    {
-        $keys = array_keys($json);
-        $results = [];
-
-        // first, check for polymorphic types...
-        if (array_key_exists('allOf', $keys)) {
-            $results[] = $this->pretty_print_row('allOf', "", $key, "", $indent_level);
-            foreach ($json['allOf'] as $schema_object) {
-                $results = array_merge($results, $this->pretty_print($schema_object, $key, [], ++$indent_level));
-            }
-            return $results;
-        } elseif (array_key_exists('anyOf', $keys)) {
-            $results[] = $this->pretty_print_row('anyOf', "", $key, "", $indent_level);
-            foreach ($json['anyOf'] as $schema_object) {
-                $results = array_merge($results, $this->pretty_print($schema_object, $key, [], ++$indent_level));
-            }
-            return $results;
-        } elseif (array_key_exists('oneOf', $keys)) {
-            $results[] = $this->pretty_print_row('oneOf', "", $key, "", $indent_level);
-            foreach ($json['oneOf'] as $schema_object) {
-                $results = array_merge($results, $this->pretty_print($schema_object, $key, [], ++$indent_level));
-            }
-            return $results;
-        } elseif (isset($json['type'])) { // then, check for all other types...
-            // use "types array" to cover simple and mixed type cases
-            $types = [];
-            if (!is_array($json['type'])) {
-                $types = [$json['type']];
-            } else {
-                $types = $json['type'];
-            }
-
-            // is "null" type used?
-            $nullable = false;
-            $null_index = array_search("null", $types);
-            if ($null_index) {
-                $nullable = true;
-                unset($types[$null_index]);
-            }
-
-            // is this required?
-            $required = false;
-            if (in_array($key, $required_keys)) {
-                $required = true;
-            }
-
-            // compute key modifiers
-            $key_modifier = ($nullable) ?
-                (($required) ? "?*" : "?") :
-                (($required) ? "*" : "");
-
-            // handle each type — could have multiple types per key
-            foreach ($types as $type) {
-                switch ($type) {
-                    case "object":
-                        $additional_properties = false;
-                        if (isset($json['additionalProperties'])) {
-                            if (is_bool($json['additionalProperties'])) {
-                                $additional_properties = $json['additionalProperties'];
-                            }
-                        }
-
-                        $results[] = $this->pretty_print_row(
-                            'object',
-                            ($additional_properties) ? " ++" : "",
-                            $key,
-                            $key_modifier,
-                            $indent_level
-                        );
-
-                        $indent_level = ++$indent_level;
-                        foreach ($json['properties'] as $key => $property) {
-                            if (isset($json['required'])) {
-                                $results = array_merge($results, $this->pretty_print($property, $key, $json['required'], $indent_level));
-                            } else {
-                                $results = array_merge($results, $this->pretty_print($property, $key, [], $indent_level));
-                            }
-                        }
-                        break;
-                    case "array":
-                        $results[] = $this->pretty_print_row('array', "", $key, $key_modifier, $indent_level);
-                        $results = array_merge($results, $this->pretty_print($json['items'], "", [], ++$indent_level));
-                        break;
-                    default:
-                        $results[] = $this->pretty_print_row($type, "", $key, $key_modifier, $indent_level);
-                        break;
-                }
-            }
-
-            return $results;
-        }
-    }
-
-    protected function pretty_print_row($type, $type_modifier = "", $key = "", $key_modifier = "", $indent_level = 0)
-    {
-        $key_final = (empty($key_modifier)) ? $key : $key.$key_modifier;
-        $type_final = (empty($type_modifier)) ? $type : $type.$type_modifier;
-
-        if (empty($key)) {
-            return str_repeat("    ", $indent_level).$type_final;
-        } else {
-            return str_repeat("    ", $indent_level).$key_final.": ".$type_final;
         }
     }
 
@@ -309,5 +188,188 @@ class ResponseValidator extends AbstractValidator
         }
 
         return $flat;
+    }
+
+    /**
+     * Helper functions for displaying a validation error.
+     */
+
+    protected function validation_error_message($schema, $validation_error)
+    {
+        $error_formatted = $this->format_validation_error($validation_error, false);
+
+        // Create a map of errors using their location.
+        $error_map = [];
+        foreach ($error_formatted['errors'] as $sub_error) {
+            $error_map[$sub_error['instanceLocation']] = $sub_error;
+        }
+
+        // Convert expected schema into an array for processing.
+        $schema = json_decode(json_encode($schema), true);
+
+        // Create a structured map of strings representing the schema.
+        $schema_formatted = $this->format_schema($schema, "#", "", [], 0);
+
+        // Display each item in the schema map. If the item is also
+        // the location of a matching error, then display it too.
+        $strings = [];
+        foreach ($schema_formatted as $key => $schema_item) {
+            if (isset($error_map[$key])) {
+                $strings[] = $schema_item.' <== '.$error_map[$key]['error'];
+            } else {
+                $strings[] = $schema_item;
+            }
+        }
+
+        // Flat display of errors
+        $error_flat = join("\n", $this->format_validation_error($validation_error, true));
+
+        // return "---\n\n".$error_flat."\n\n".join("\n", $strings)."\n\n  ---";
+        return "---\n\n".join("\n", $strings)."\n\n  ---";
+    }
+
+    protected function format_validation_error($validation_error, $flat = false)
+    {
+        $formatter = new ErrorFormatter();
+
+        return ($flat) ? $formatter->formatFlat($validation_error) :
+            $formatter->formatOutput($validation_error, "basic");
+    }
+
+    /**
+     * @param array $schema JSON schema represented as an array
+     * @param string $location The current location/path within the JSON schema structure
+     * @param string $key_current The key at the current location
+     * @param array $keys_required The keys required at the current location
+     * @param int $indent_level
+     * @return array
+     */
+    protected function format_schema($schema, $location, $key_current, $keys_required, $indent_level)
+    {
+        $keys = array_keys($schema);
+        $results = [];
+
+        // first, check for polymorphic types...
+        if (array_key_exists('allOf', $keys)) {
+            $location .= '/allOf';
+            $content = $this->expected_schema_row_content('allOf', "", $key_current, "");
+            $results[$location] = $this->expected_schema_row($content, $indent_level);
+
+            foreach ($schema['allOf'] as $schema_object) {
+                $results = array_merge($results, $this->format_schema($schema_object, $location, $key_current, [], ++$indent_level));
+            }
+
+            return $results;
+        } elseif (array_key_exists('anyOf', $keys)) {
+            $location .= '/anyOf';
+            $content = $this->expected_schema_row_content('anyOf', "", $key_current, "");
+            $results[$location] = $this->expected_schema_row($content, $indent_level);
+
+            foreach ($schema['anyOf'] as $schema_object) {
+                $results = array_merge($results, $this->format_schema($schema_object, $location, $key_current, [], ++$indent_level));
+            }
+
+            return $results;
+        } elseif (array_key_exists('oneOf', $keys)) {
+            $location .= '/oneOf';
+            $content = $this->expected_schema_row_content('oneOf', "", $key_current, "");
+            $results[$location] = $this->expected_schema_row($content, $indent_level);
+
+            foreach ($schema['oneOf'] as $schema_object) {
+                $results = array_merge($results, $this->format_schema($schema_object, $location, $key_current, [], ++$indent_level));
+            }
+
+            return $results;
+        } elseif (isset($schema['type'])) { // then, check for all other types...
+            // use "types array" to cover simple and mixed type cases
+            $types = [];
+            if (!is_array($schema['type'])) {
+                $types = [$schema['type']];
+            } else {
+                $types = $schema['type'];
+            }
+
+            // is "null" type used?
+            $nullable = false;
+            $null_index = array_search("null", $types);
+            if ($null_index) {
+                $nullable = true;
+                unset($types[$null_index]);
+            }
+
+            // is this required?
+            $required = false;
+            if (in_array($key_current, $keys_required)) {
+                $required = true;
+            }
+
+            // compute key modifiers
+            $key_modifier = ($nullable) ?
+                (($required) ? "?*" : "?") :
+                (($required) ? "*" : "");
+
+            // compute next location
+            if ($key_current !== "") {
+                $location .= '/'.$key_current;
+            }
+
+            // handle each type — could have multiple types per key
+            foreach ($types as $type) {
+                switch ($type) {
+                    case "object":
+                        $additional_properties = true;
+                        if (isset($schema['additionalProperties'])) {
+                            if (is_bool($schema['additionalProperties'])) {
+                                $additional_properties = $schema['additionalProperties'];
+                            }
+                        }
+
+                        $content = $this->expected_schema_row_content(
+                            'object',
+                            ($additional_properties) ? "++" : "",
+                            $key_current,
+                            $key_modifier
+                        );
+                        $results[$location] = $this->expected_schema_row($content, $indent_level);
+
+                        $indent_level = ++$indent_level;
+                        foreach ($schema['properties'] as $key => $property) {
+                            if (isset($schema['required'])) {
+                                $results = array_merge($results, $this->format_schema($property, $location, $key, $schema['required'], $indent_level));
+                            } else {
+                                $results = array_merge($results, $this->format_schema($property, $location, $key, [], $indent_level));
+                            }
+                        }
+                        break;
+                    case "array":
+                        $content = $this->expected_schema_row_content('array', "", $key_current, $key_modifier);
+                        $results[$location] = $this->expected_schema_row($content, $indent_level);
+
+                        $results = array_merge($results, $this->format_schema($schema['items'], $location.'/1', "", [], ++$indent_level));
+
+                        break;
+                    default:
+                        $content = $this->expected_schema_row_content($type, "", $key_current, $key_modifier);
+                        $results[$location] = $this->expected_schema_row($content, $indent_level);
+
+                        break;
+                }
+            }
+
+            return $results;
+        }
+    }
+
+    protected function expected_schema_row_content($type, $type_modifier = "", $key = "", $key_modifier = "")
+    {
+        $key_final = $key.$key_modifier;
+        $type_final = $type.$type_modifier;
+
+        return (empty($key)) ? $type_final : $key_final.": ".$type_final;
+    }
+
+    protected function expected_schema_row($display, $indent_level = 0)
+    {
+        return str_repeat("    ", $indent_level).$display;
     }
 }
