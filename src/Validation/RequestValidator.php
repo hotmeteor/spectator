f1e369c8a9a2ce0d4346e33e01e703583d58591a
Dan Evans <danevans485@gmail.com>
2023-07-28 15:00:58 +0100

    Adds support for required, readOnly and writeOnly properties (#147)
    * chore: readonly writeonly test schemas
    
    * test: adds readonly tests
    
    * test: adds writeonly tests
    
    * feat: filter readonly and writeonly properties
    
    * fix: adds supports for anyOf, allOf and oneOf
    
    * fix: styling and debug
    
    * chore: styling
    
    * chore: removes extra space

--- src/Validation/RequestValidator.php
+++ src/Validation/RequestValidator.php
@@ -3,7 +3,6 @@
 namespace Spectator\Validation;
 
 use cebe\openapi\spec\Operation;
-use cebe\openapi\spec\Parameter;
 use cebe\openapi\spec\PathItem;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
@@ -202,7 +201,7 @@ class RequestValidator extends AbstractValidator
                 $actual_body_schema = $this->parseBodySchema();
             }
         }
-        $expected_body_schema = $this->prepareData($expected_body_raw_schema);
+        $expected_body_schema = $this->prepareData($expected_body_raw_schema, 'write');
 
         // Run validation.
         $validator = new Validator();
