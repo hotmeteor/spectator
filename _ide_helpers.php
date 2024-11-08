b1609717dbb1c1e039a9758b7015aa64645e4906
Fabien Villepinte <fabien.villepinte@gmail.com>
2024-11-08 16:09:48 +0100

    fix: Update _ide_helpers.php

--- _ide_helpers.php
+++ _ide_helpers.php
@@ -8,14 +8,14 @@ namespace Illuminate\Testing {
     /**
      * @see \Spectator\Assertions
      *
-     * @method static assertValidRequest()
-     * @method static assertInvalidRequest()
-     * @method static assertValidResponse($status = null)
-     * @method static assertInvalidResponse($status = null)
-     * @method static assertValidationMessage($expected)
-     * @method static assertErrorsContain($errors)
-     * @method static dumpSpecErrors()
-     * @method static void skipRequestValidation()
+     * @method $this assertValidRequest()
+     * @method $this assertInvalidRequest()
+     * @method $this assertValidResponse($status = null)
+     * @method $this assertInvalidResponse($status = null)
+     * @method $this assertValidationMessage($expected)
+     * @method $this assertErrorsContain($errors)
+     * @method $this assertPathExists()
+     * @method $this dumpSpecErrors()
      */
     class TestResponse
     {
